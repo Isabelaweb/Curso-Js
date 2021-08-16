ef57b99feb6ce50556037d21da5992228995d0fe
isabela <isabelaevellyn20@gmail.com>
2021-08-16 17:38:14 -0300

    Curso de Js

---
+++ desafio-01/desafio.js
@@ -0,0 +1,12 @@
+// 1º Criar balão 
+// 2° Estourar balão
+// 3º Carregar o jogo
+
+function criarBalao() {
+    var balao = document.createElement("div");
+    balao.setAttribute("class", "balao");
+
+    var x = Math.random()
+
+
+}
--- js-intermediario/curso-script2.0.js
+++ js-intermediario/curso-script2.0.js
@@ -26,3 +26,49 @@ function onclick() {
 function botaoDireito() {
     alert("Você clickou com botão direito")
 }
+
+/////// 
+function tecladoApertado(event) {
+    console.log("Teclado Apertado" + event.keyCode); // codigo da tecla que retorna um número
+    if (event.keyCode == 32) {
+        console.log("Você apertou o espaço")
+    } else {
+        console.log("Aperte o espaço")
+    }
+}
+
+function shift(recebeUmEvento) {
+    console.log("Shift ainda não precionado" + recebeUmEvento.shiftKey) // Quando o shif for apertado ele retorna um boleam com true.
+    if (recebeUmEvento.shiftKey == true) {
+        console.log("Shift precionado");
+    }
+
+}
+
+function soltouOteclado() {
+    console.log("Soltou o teclado");
+}
+
+function apertouEsoltouMouse() {
+    console.log("Apertou alguma tecla");
+}
+
+function carregou() {
+    alert("Pagina carregada");
+}
+
+function focado() {
+    console.log("Campo focado");
+}
+
+function desfocado() {
+    console.log("Desfocou do campo");
+}
+
+function opcaoSelecionada(objeto) {
+    console.log("Cidade selecionada: " + objeto.value);
+}
+
+function enviado() {
+    console.log("Formulario Enviado com Sucesso !!!")
+}
